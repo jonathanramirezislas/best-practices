bf42007d00d1279666075c480c406b48a5fe4bc6
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-03-09 16:52:14 -0600

    avoidNullPointException

--- src/nullpoinyexception/Main.java
+++ src/nullpoinyexception/Main.java
@@ -2,11 +2,33 @@ package nullpoinyexception;
 
 public class Main {
     public static void main(String[] args) {
-     
-        String name =null;
 
-        //System.out.println(name.equals("jonathan"));// will launch a error NullPointException
+        String name = null;
+
+        // System.out.println(name.equals("jonathan"));// will launch a error
+        // NullPointException
         System.out.println("jonathan".equals(name));
+        
+        /**
+         * JAVA 8
+            Address defaultAddress = new Address (....); 
+            Optional<Address> optAddress =user.getAddress();
+            Address address = optAddress.orElse(defaultAddress);
+        
+            Optional<Address> optAddress = user.getAddress();
+            Address address = optAddress.orElseThrow(UserNotFoundException::new)
+        
+
+            Address address;
+            if(optAddress.isPresent){
+            address = optAddress.get();
+            }
+
+            Optional<Address> optAddress = user.getAddress();
+            optAddress.ifPresent(System.out::println);
+          
+            */
+
 
     }
 }
