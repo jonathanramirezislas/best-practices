5835571500207a2193d2b5ef062772b0cf7274ea
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-03-21 17:44:08 -0600

    Decorator ejemplo 2

---
+++ src/decoratorEjemplo2/Configurable.java
@@ -0,0 +1,6 @@
+package decoratorEjemplo2;
+
+public interface Configurable {
+    float getPrecioBase();
+    String getIngredientes();
+}
---
+++ src/decoratorEjemplo2/decorador/CafeDecorador.java
@@ -0,0 +1,11 @@
+package decoratorEjemplo2.decorador;
+
+import decoratorEjemplo2.Configurable;
+
+abstract public class CafeDecorador implements Configurable {
+    protected Configurable cafe;
+
+    public CafeDecorador(Configurable cafe) {
+        this.cafe = cafe;
+    }
+}
